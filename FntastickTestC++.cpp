ed9e65a356b1e3212dee96b665186a5817c8bf2f
Denis Pochuev <denis.pochuev@gmail.com>
2022-09-08 10:32:18 +0400

    Залил задание.

---
+++ FntastickTestC++.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+void сase_alignment(std::string& text);
+void сharacter_substitution(std::string& text);
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	std::string text{};
+
+	getline(std::cin, text);
+	сase_alignment(text);
+	сharacter_substitution(text);
+
+	std::cout << text;
+}
+
+void сase_alignment(std::string& text)
+{
+	for (char& i : text)
+	{
+		if (i >= 'A' && i <= 'Z') { i += 32; }
+	}
+}
+
+void  сharacter_substitution(std::string& text)
+{
+	const int length = static_cast<int>(text.length());
+
+	for (int i = 0; i < length; i++)
+	{
+		bool flag_repeat = false;
+		const char replacement_character = text[i];
+
+		for (int j = i + 1; j < length; j++)
+		{
+			if (replacement_character == text[j])
+			{
+				text[j] = ')';
+				text[i] = ')';
+				flag_repeat = true;
+			}
+		}
+
+		if (text[i] != ')' && text[i] != '(') { flag_repeat ? text[i] = ')' : text[i] = '('; }
+	}
+}
